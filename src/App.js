031c758e0c505b0ba95db464d3b0b1ce1bfa9dac
OriusMJC <matiasjesuscontreras12@hotmail.com>
2022-05-30 21:58:34 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {useEffect,useState} from 'react';
 
 function App() {
-  const en = ['It is certain.', 'It is decidedly so.', 'Without a doubt', 'Yes definitely.',
+const en = ['It is certain.', 'It is decidedly so.', 'Without a doubt', 'Yes definitely.',
 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 
 'Signs point to yes.','Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.',
 'Cannot predict now.', 'Concentrate and ask again.', "Don't count on it", 'My reply is no.',
